9e0afcf8992c814eaa23eab66dcf55400ce9ebbe
AugustoDC <dan.correia10@hotmail.com>
2020-07-09 19:36:25 -0300

    adicionando service de validacao do formulario

---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    return ((control.dirty || control.touched) && this.hasError(control, errorName));
+  }
+
+  hasError(control: AbstractControl, errorName: string): boolean {
+    return control.hasError(errorName);
+  }
+}
